a0ea0ec63f74af52ddce9e891368483f339dd36e
Ninamabad <151802511+Ninamabad@users.noreply.github.com>
2025-02-16 23:21:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,18 +42,18 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY', True)
+IS_VERIFY = is_enabled('IS_VERIFY', flase)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "0")
 VERIFY_IMG = environ.get("VERIFY_IMG", "0")
-SHORTENER_API = environ.get("SHORTENER_API", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'omegalinks.in')
-SHORTENER_API2 = environ.get("SHORTENER_API2", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'omegalinks.in')
-SHORTENER_API3 = environ.get("SHORTENER_API3", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'omegalinks.in')
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
+SHORTENER_API = environ.get("SHORTENER_API", "0")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", '0')
+SHORTENER_API2 = environ.get("SHORTENER_API2", "0")
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", '0')
+SHORTENER_API3 = environ.get("SHORTENER_API3", "0")
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", '0')
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "0"))
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "0"))
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi"]
